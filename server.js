46c074f71f222c86884864205be21728540fb94c
Gilson-chipombo <fernandochipombo@gmail.com>
2025-12-06 16:42:31 +0100

    FEAT: generateToken and authToken function generated

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const dotenv  = require('dotenv');
 const cors    = require('cors');
 const rateLimit = require('express-rate-limit');
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+const BCRYPT_SALT = parseInt(process.env.BCRYPT_SALT || '10');
+const JWT_SECRET  = process.env.JWT_SECRET;
+
 const rateRequestLimit = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 50,
@@ -14,8 +20,41 @@ const rateRequestLimit = rateLimit({
 
 app.use(cors({origin: process.env.CORS_ORIGIN || '*'}));
 app.use(helmet());
-app.use(rateRequestLimit());
-dotenv.config();
+app.use(rateRequestLimit);
+app.use(express.json({limit: '100kb'}));
 
-const PORT = process.env.PORT || 3000;
-const BCRYPT_SALT = parseInt(process.env.BCRYPT_SALT || '10');
+
+
+function generateToken(user)
+{
+    const payload = {id: user.id, name: user.name, number: user.name, role: user.role};
+    return jwt.sign(payload, JWT_SECRET, {expiresIn: '15m'});
+}
+
+function auth(req, res, next)
+{
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) return res.status(401).json({error: 'Invalid request. Token is missing'});
+
+    try{
+        const payload =  jwt.verify(token, JWT_SECRET);
+        req.user = payload;
+        next();
+    }catch(error)
+    {
+        res.status(401).json({error: "Token Invalid"});
+    }
+}
+
+function login(req, res, next){
+    res.status(200).json({message: "Hello Campeao do CodeFast Africell"});
+}
+
+app.post('/api/login', login);
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
